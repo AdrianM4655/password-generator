5880a5cdf5a35c4e67a446689de0b59526c0813b
Adrian Magana <adrianberkeley56@gmail.com>
2021-09-23 16:02:48 -0700

    add promt and error properties

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var password = "";
+
+  var passwordLength = prompt("How long would you like your password? (must be between 8-128 characters)");
+    if (passwordLength.length >= 8 && passwordLength.length <= 128) 
+    {
+      return true;
+    }
+    else 
+    {
+      alert("Must be between 8 - 128 charecters! Please try again!");
+      return false;
+    } 
+}
