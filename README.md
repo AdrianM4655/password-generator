b5ae04e7d50bd8fe8feb24f1a7447d41868c1476
Adrian Magana <adrianberkeley56@gmail.com>
2021-09-24 05:19:57 -0700

    correcting java, adding readme.md

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
-# password-generator
+
+#Password Generator 
+
+A program where the user can input what type of attributes they would like in their code, including the length of it, and then give the user a random generated password that fits their criteria!
+
+## Demo/Deployed Link
+
+https://shamika69.github.io/password-generator/
+
+## Screenshots
+
+![Site Screenshot](https://media.giphy.com/media/StIxB4RLNtLEPEC3sA/giphy.gif?cid=790b761167cbac382a8237b355d71c2ead84c92269d8046d&rid=giphy.gif&ct=g)
+
+  
+## Installation
+
+Install 'professional portfolio' with github fork function (tab down of green code button)
+
+```bash
+  cd to desktop
+  git clone <link to my repository>
+  cd to professional-portfolio
+  open by using 'code .' should send you to visual studios code
+  edit code to liking!
+```
+    
+## Built With
+- [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+- [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
+- [Java](https://www.javascript.com/)
+
+## Authors
+
+- Adrian Magana
+ - [Link to Portfolio Site](https://shamika69.github.io/professional-portfolio/)
+- [Link to Github](https://github.com/shamika69)
+- [Link to LinkedIn](https://www.linkedin.com/in/adrian-magana/)
+  
+## License
+
+(UCB)
+
+## Code Snippit
+
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+//sets up string of numbers
+function numbersRan () {
+  var numbersRandomChar = "0123456789".split("");
+  return numbersRandomChar;
+}
+
+//sets up string of special characters   
+function signsRan () {
+  var signsRandomChar = "></^$#!@*,)(+=~".split("");
+  return signsRandomChar;
+}
+   
+//sets up string of lowercase letters
+function lettersLRan () {
+  var lettersLRandomChar = "abcdefghijklmnopqrstuvwxyz".split("");
+  return lettersLRandomChar;
+}
+  
+//sets up string of uppercase letters
+function lettersURan () {
+  var lettersURandomChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  return lettersURandomChar;
+}
--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -7,23 +6,75 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//sets up string of numbers
+function numbersRan () {
+  var numbersRandomChar = "0123456789".split("");
+  return numbersRandomChar;
+}
+
+//sets up string of special characters   
+function signsRan () {
+  var signsRandomChar = "></^$#!@*,)(+=~".split("");
+  return signsRandomChar;
+}
+   
+//sets up string of lowercase letters
+function lettersLRan () {
+  var lettersLRandomChar = "abcdefghijklmnopqrstuvwxyz".split("");
+  return lettersLRandomChar;
+}
+  
+//sets up string of uppercase letters
+function lettersURan () {
+  var lettersURandomChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  return lettersURandomChar;
+}
+
+//function to cover the gerneration of the password
 function generatePassword() {
   var password = "";
 
-  var passwordLength = prompt("How long would you like your password? (must be between 8-128 characters)");
-    if (passwordLength.length >= 8 && passwordLength.length <= 128) 
-    {
-      return true;
+  //the user is prompted to input lenght of password
+  var passLength = prompt("How long would you like your password? (must be between 8-128 characters)")
+
+  //asks the user for diffrent criteria they want in theyre password
+  if (passLength >=8 && passLength <= 128) {
+    var numberChar = confirm("Would you like a password with numbers?, press ok! If not, select cancel!");
+    var symbolChar = confirm("Would you like a password with signs?, press ok! If not, select cancel!");
+    var lowLettersChar = confirm("Would you like a password with lower case letters?, press ok! If not, select cancel!");
+    var upLettersChar = confirm("Would you like a password with upper case letters?, press ok! If not, select cancel!");
+
+    //merdges all of the character sets together
+    if (numberChar || symbolChar || lowLettersChar || upLettersChar) {
+      var allChar = [];
+      if (numberChar) {
+        allChar = allChar.concat(numbersRan());
+      }
+      if (symbolChar) {
+        allChar = allChar.concat(signsRan());
+      }
+      if (lowLettersChar) {
+        allChar = allChar.concat(lettersLRan());
+      }
+      if (upLettersChar) {
+        allChar = allChar.concat(lettersURan());
+      }
+      //randomly generates a character and repeats til criteria for number of characters is met
+      console.log(allChar);
+      for (var i = 0; i < passLength; i++) {
+        password = password + allChar[Math.floor(Math.random() * allChar.length)];
+      }
+      //gives and displays final password
+      return password;
     }
-    else 
-    {
-      alert("Must be between 8 - 128 charecters! Please try again!");
-      return false;
-    } 
+  //if no character is selected, asks user to try again
+  return "Please select minimal one charecter! Try Again!"
+  }
+  //if character number asked is less than 8 or more than 128 the user is asked to try again
+  return "Input must be between 8 to 128 charecters! Please Try Again!"
 }
